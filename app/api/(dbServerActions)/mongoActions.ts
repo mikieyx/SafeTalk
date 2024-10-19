9586e02a38e9a008ac292d9be8203f1275fdefb6
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-10-19 16:43:43 -0700

    add function with unique user information too user specific config

--- app/api/(dbServerActions)/mongoActions.ts
+++ app/api/(dbServerActions)/mongoActions.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { CallOptions, systemPrompt } from "../vapiAgentUtils";
 
-export default async function getContactCallOptions(
+export async function getContactCallOptions(
   contactId: string
 ): Promise<Partial<CallOptions> | null> {
   "use server";
@@ -22,6 +22,40 @@ export default async function getContactCallOptions(
     customer: { number: assistant.user_phone_number },
     assistant: {
       model: {
+        tools: [
+          {
+            type: "function",
+            messages: [
+              {
+                type: "request-start",
+                content:
+                  "I'm going to notify your emergency contacts of the ongoing conversation. Please wait...",
+              },
+              {
+                type: "request-complete",
+                content:
+                  "I have notified your emergency contacts of the ongoing conversation.",
+              },
+              {
+                type: "request-failed",
+                content:
+                  "I couldn't notify your emergency contacts of the ongoing conversation.",
+              },
+            ],
+            function: {
+              name: "transcript_logger",
+              parameters: {
+                type: "object",
+                properties: {},
+              },
+              description: "Logs the transcript of the current call.",
+            },
+            async: false,
+            server: {
+              url: `https://safewords.us/api/notifyContact/${assistant.user_phone_number}`,
+            },
+          },
+        ],
         messages: [
           {
             content: systemPrompt(
