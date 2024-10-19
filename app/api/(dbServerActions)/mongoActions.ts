3b916203bb0a97665e1fae03d3d287bf2c1c6627
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-10-19 11:05:03 -0700

    merge

--- app/api/(dbServerActions)/mongoActions.ts
+++ app/api/(dbServerActions)/mongoActions.ts
@@ -1,10 +1,18 @@
-import { CallOptions } from "../callContact/[contactid]/route";
+import prisma from "@/lib/prisma";
+import { CallOptions } from "../vapiAgentUtils";
 
 export default async function getContactCallOptions(
   contactId: string
 ): Promise<Partial<CallOptions>> {
   "use server";
+  const assistant = await prisma.assistant.findFirst({
+    where: {
+      id: contactId,
+    },
+  });
+
   return Promise.resolve({
     name: contactId,
+    
   });
 }
