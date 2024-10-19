26e7c3cdeedbe1f3a0dc7c5149ab26b4b34905aa
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-10-18 21:58:29 -0700

    Create user in database if not exist

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = (await currentUser());
+
+  if (!user) {
+    return null;
+  }
+
+  const phoneNumber = user.primaryPhoneNumber!.phoneNumber;
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      phone_number: phoneNumber
+    }
+  });
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+          phone_number: phoneNumber
+      }
+    });
+  }
+
   return <div className="">{/* Contacts Here */}</div>;
 }
