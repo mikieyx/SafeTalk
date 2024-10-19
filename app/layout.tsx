974c195b4b29658b4c0444ef87632b994e173df5
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-10-18 21:58:07 -0700

    Force redirect to sign in if not signed in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +31,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <SignedOut>
-            <SignInButton/>
+            <RedirectToSignIn/>
           </SignedOut>
           <SignedIn>
             <UserButton/>
